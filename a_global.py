6b80a538fc359aad6e38f70e655f2e321d4284fd
killbot <davidhoman314@gmail.com>
2011-04-12 10:28:42 -0500

    added letters, upper and lowercase

--- a_global.py
+++ a_global.py
@@ -1,9 +1,16 @@
 import pygame
 import sys
+from math import sqrt
 
-font_size = 64
+font_size = 300
 frame_rate = 30
 screen_size = (800, 600)
 letter_change_freq = 2  #number of seconds until next letter displayed
 screen = pygame.display.set_mode(screen_size)
 font_color = (255, 255, 255)
+letter_velocity = 20   #in pixels per frame
+cos_dict = {0:1, 45:sqrt(2)/2, 90:0, 135:-sqrt(2)/2, 180:-1, 225:-sqrt(2)/2, \
+    270:0, 315:sqrt(2)/2}
+sin_dict = {0:0, 45:sqrt(2)/2, 90:1, 135:sqrt(2)/2, 180:0, 225:-sqrt(2)/2, \
+    270:-1, 315:-sqrt(2)/2}
+
--- alphabet0.py
+++ alphabet0.py
@@ -23,6 +23,9 @@ class Alphabet(object):
         global frame_rate
         global font_size
         global font_color
+        self.background = pygame.Surface(screen_size)
+        self.background = self.background.convert()
+        self.background.fill((0, 0, 0))
         self.my_font = pygame.font.Font(None, font_size)
         self.playing = True
         self.letter_list_uc = ["A", "B", "C", "D", "E", "F", "G", "H", "I"\
@@ -31,18 +34,27 @@ class Alphabet(object):
         self.letter_list_lc = []
         self.temp_counter =0
         for i in self.letter_list_uc:
-            temp_string = string.lower(i)
-            self.letter_list_lc[self.temp_counter] = \
-                self.my_font.render(temp_string, 1, font_color)
-            self.letter_
+            temp_string_lc = string.lower(i)
+            temp_string_uc = self.letter_list_uc[self.temp_counter]
+            self.letter_list_lc.append(self.my_font.render(temp_string_lc, 1,\
+                font_color))
+#            self.letter_list_lc[self.temp_counter] = \
+#                self.my_font.render(temp_string_lc, 1, font_color)
+            self.letter_list_uc[self.temp_counter] = \
+                self.my_font.render(temp_string_uc, 1, font_color)
             self.temp_counter += 1
         self.letter_list = [self.letter_list_uc, self.letter_list_lc]
         self.current_letter = 0     #index of the current letter
         self.current_caps = 0       # 0 = uppercase, 1 = lowercase
-        self.letter_counter = 0
+        self.letter_counter = 0     #used for animating from letter to letter
     def update(self):
-        if self.letter_counter >= frame_rate * letter_change_freq:
+        if self.letter_counter >= (frame_rate * letter_change_freq):
             self.letter_counter = 0
+            self.current_letter += 1
+            if self.current_letter >= len(self.letter_list_uc):
+                self.current_letter = 0
+        print("counter and letter = ", self.letter_counter, self.current_letter)
+        self.letter_counter += 1
 
     
 
@@ -50,11 +62,18 @@ class Alphabet(object):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
-
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    if self.current_caps == 0:      #this is the logical
+                        self.current_caps = 1       #XOR operation on 
+                    elif self.current_caps == 1:    #the current_caps flag
+                        self.current_caps = 0       #variable (0 or 1)
         return self.playing
 
     def draw(self):
-        pass
+        screen.blit(self.background, (0,0))
+        screen.blit(self.letter_list[self.current_caps][self.current_letter], (0,0))
+        pygame.display.flip()
 
 
 
